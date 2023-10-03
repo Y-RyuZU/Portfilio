71467532b6e42e47bddbb9f793a4f70bffe30fbb
Y_RyuZU <ryuzu1006@gmail.com>
2023-10-03 15:09:13 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+    <v-app>
+        <Header/>
+        <RouterView/>
+    </v-app>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <v-app-bar app>
+        <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>My App</v-app-bar-title>
+
+        <template v-slot:append>
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+        </template>
+    </v-app-bar>
+</template>
+
+<style scoped>
+
+</style>
